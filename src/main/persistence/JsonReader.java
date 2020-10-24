6e003695c0618124ccd6562b0603d83d8785ec34
Adrian Pikor <loler88@hotmail.ca>
2020-10-24 00:24:23 -0700

    Phase 2, Task 4, 5 (partial) - Added JsonReader and JsonWriter classes to read and write JSON data of the program's workspace (heavily modelled after classes of the same name in JsonSerializationDemo, cited appropriately)

---
+++ src/main/persistence/JsonReader.java
@@ -0,0 +1,88 @@
+package persistence;
+
+import model.Function;
+import model.Workspace;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.stream.Stream;
+
+// Represents a reader that reads workroom from JSON data stored in file
+public class JsonReader {
+    private String source;
+
+    // EFFECTS: constructs reader to read from source file
+    public JsonReader(String source) {
+        this.source = source;
+    }
+
+    // EFFECTS: reads workroom from file and returns it;
+    // throws IOException if an error occurs reading data from file
+    public Workspace read() throws IOException {
+        String jsonData = readFile(source);
+        JSONObject jsonObject = new JSONObject(jsonData);
+        return parseWorkspace(jsonObject);
+    }
+
+    // EFFECTS: reads source file as string and returns it
+    private String readFile(String source) throws IOException {
+        StringBuilder contentBuilder = new StringBuilder();
+
+        try (Stream<String> stream = Files.lines(Paths.get(source), StandardCharsets.UTF_8)) {
+            stream.forEach(s -> contentBuilder.append(s));
+        }
+
+        return contentBuilder.toString();
+    }
+
+    // REQUIRES: jsonObject is the JSONObject of the whole data file
+    // MODIFIES: Workspace
+    // EFFECTS: parses workspace from JSON object and returns it
+    private Workspace parseWorkspace(JSONObject jsonObject) {
+        //String name = jsonObject.getString("name");
+        Workspace workspace = new Workspace();
+        populateWorkspace(workspace, jsonObject);
+        return workspace;
+    }
+
+    // REQUIRES: workspace must be empty, workspaceJson is the JSONObject representing the workspace
+    // MODIFIES: Workspace
+    // EFFECTS: populates the workspace with JSON data stored in workspaceJson
+    private void populateWorkspace(Workspace workspace, JSONObject workspaceJson) {
+        JSONArray funcArray = workspaceJson.getJSONArray("functionList");
+        for (int i = 0; i < funcArray.length(); i++) {
+            JSONObject funcJson = funcArray.getJSONObject(i);
+            HashMap<String, Double> constants = new HashMap<>();
+            ArrayList<Double> domain = new ArrayList<>();
+
+            //populate constants and domain
+            populateConstantsAndDomain(funcJson, constants, domain);
+
+            //create Function object
+            Function func = new Function(funcJson.getString("type"), constants, domain);
+
+            //add to workspace
+            workspace.addFunction(func, funcJson.getString("name"));
+        }
+    }
+
+    // REQUIRES: funcJson is a JSONObject representing a Function object,
+    //           constants is empty, domain is empty
+    // EFFECTS: populates constants HashMap and domain ArrayList with values stored in the funcJson JSONObject
+    private void populateConstantsAndDomain(JSONObject funcJson, HashMap<String, Double> constants,
+                                            ArrayList<Double> domain) {
+        JSONObject constantsJson = funcJson.getJSONObject("constants");
+        JSONArray domainJson = funcJson.getJSONArray("domain");
+        for (String constKey : constantsJson.keySet()) {
+            constants.put(constKey, constantsJson.getDouble(constKey));
+        }
+        domain.add(domainJson.getDouble(0));
+        domain.add(domainJson.getDouble(1));
+    }
+}
